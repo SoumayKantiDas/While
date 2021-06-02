c5944081661874df110d3974ee545cd96fe97471
Soumay kanti Das <anikds2859@outlook.com>
2021-06-02 14:11:06 +0600

    Add project files.

---
+++ While/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace While
+{
+    class WhileTest
+    {
+        static void Main(string[] args)
+        {
+            int n = 1;
+            while (n < 6)
+            {
+                Console.WriteLine("Current value of n is {0}",n);
+                n++;
+            }
+            
+        }
+    }
+}
